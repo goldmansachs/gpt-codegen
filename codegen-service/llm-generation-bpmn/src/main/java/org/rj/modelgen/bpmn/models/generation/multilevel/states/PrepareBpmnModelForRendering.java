90979cd64aeb821566ebfec4521f6722146ca945
Liput, Milosz [Engineering] <Milosz.Liput@ln.email.gs.com>
2026-05-21 14:33:35 +0100

    cleanup

--- codegen-service/llm-generation-bpmn/src/main/java/org/rj/modelgen/bpmn/models/generation/multilevel/states/PrepareBpmnModelForRendering.java
+++ codegen-service/llm-generation-bpmn/src/main/java/org/rj/modelgen/bpmn/models/generation/multilevel/states/PrepareBpmnModelForRendering.java
@@ -193,14 +193,6 @@ public class PrepareBpmnModelForRendering extends PrepareModelForRendering {
         }
     }
 
-    protected void saveStartingPayload(BpmnModelAssets modelAssets) {
-        Set<PayloadVariable> processPayload = getPayload().get(MultiLevelModelStandardPayloadData.ProcessVariables);
-        if (processPayload == null || processPayload.isEmpty()) return;
-
-        var startingPayload = processPayload.stream().toList();
-        modelAssets.setStartingPayload(startingPayload);
-    }
-
     protected String postProcessScriptInput(ElementNodeInput input, String inputValue) {
         inputValue = resolveErrorThrows(inputValue, "throw new Exception($1)");
         return inputValue;
